109a458925aff74c02368d8a4abbc998461794de
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2025-10-30 16:44:51 +0530

    M_10/30/25

---
+++ Day_1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<thread>    // this will help to create thread and manage 
+
+void task(){
+    std::cout<<"Task"<<"\n";
+}
+
+int main(){
+    std::thread t(task); // creating thread
+    t.join();  
+    
+    std::cout<<"Mian thread "<<"\n";
+    return 0;
+}
+
+
