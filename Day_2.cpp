a79db9479993ad47c2dd00ac8e4cb614fe22c32e
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2025-11-01 22:44:58 +0530

    1/10/25

---
+++ Day_2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#inclue<thread>
+using namespace std;
+
+
+void taskWithParameter(int start,int end){
+    
+    for(int i = start;i<end; i++){
+        std::cout<<"Thread with parameter "<<i<<"\n";
+    }
+}
+
+int main(){
+
+    std::thread t(taskWithParameter, 1,5);
+    t.join();
+    return 0;
+}
